90aef254699bc16a44596fbe6c32fca86665707d
br4e <milesminton@gmail.com>
2023-09-28 08:15:28 -0700

    Move aggregator

--- src/aggregator/AverageAggregator.sol
+++ src/aggregator/AverageAggregator.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import { IAggregator } from './interface/IAggregator.sol';
+import { IAggregator } from '../interface/IAggregator.sol';
 
 contract AverageAggregator is IAggregator {
     function aggregate(
