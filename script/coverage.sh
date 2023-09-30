84c9ffd1c7b01f09184a76d9474edfe818953238
br4e <milesminton@gmail.com>
2023-09-30 07:44:18 -0700

    Add test coverage report

---
+++ script/coverage.sh
@@ -0,0 +1,20 @@
+# SPDX-License-Identifier: AGPL-3.0
+
+# if any command fails then immediately exit
+set -o errexit
+
+# setup
+rm -rf coverage
+mkdir -p coverage
+
+# create coverage report
+forge coverage --report summary --report lcov 
+
+# create html viewable coverage report
+genhtml lcov.info --dark-mode -o coverage
+
+# cleanup
+rm -f coverage/lcov.info
+mv lcov.info coverage
+
+echo 'Success! Coverage data viewable in coverage/index.html'
