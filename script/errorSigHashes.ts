b2a6ecf2e8b9f72d1531b4410d157a1b79bad202
T <relyt29@users.noreply.github.com>
2023-10-03 18:33:58 -0400

    [ORA-18] Push example price so that you have something to send cross chain (#4)

---
+++ script/errorSigHashes.ts
@@ -0,0 +1,46 @@
+import { ethers } from "ethers";
+import { resolve } from "path";
+import { readFile, appendFile } from "fs";
+import { readdir } from "fs/promises";
+
+async function* getFiles(dir: string): AsyncGenerator<string> {
+  const dirents = await readdir(dir, { withFileTypes: true });
+  for (const dirent of dirents) {
+    const res = resolve(dir, dirent.name);
+    if (dirent.isDirectory()) {
+      yield* getFiles(res);
+    } else {
+      yield res;
+    }
+  }
+}
+
+async function main() {
+  for await (const f of getFiles("out/")) {
+    if (!f.endsWith(".json")) {
+      console.log(`Skipping non json file ${f}`);
+      continue;
+    }
+    readFile(f, "utf-8", (err, data) => {
+      if (err) {
+        console.error(`Error reading file ${f}: ${err}`);
+      }
+      if (data) {
+        const compilationArtifact = JSON.parse(data);
+        const abi = compilationArtifact.abi;
+        if (abi === undefined || abi === null) {
+          console.error(`No abi found for ${f}, skipping`);
+          return;
+        } else {
+          const interfaceAbi = new ethers.Interface(abi);
+          interfaceAbi.forEachError((errFragment) => {
+            const sighash = errFragment.selector;
+            console.log(`${errFragment.name} ${sighash}`)
+          });
+        }
+      }
+    });
+  }
+}
+
+main();
---
+++ script/pushPrice.s.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.21;
+
+import '../lib/forge-std/src/Script.sol';
+
+import { Prices } from '../src/Prices.sol';
+import { PriceData } from '../src/interface/IPrices.sol';
+import { ECDSA } from '../lib/openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol';
+
+struct PriceDataWithoutSignature {
+    uint256 feedId;
+    uint256 nonce;
+    uint96 timestamp;
+    uint256 price;
+    bytes extraData;
+}
+
+contract PushPrice is Script {
+    function run() public virtual {
+        uint256 privateKey = vm.envUint('DEPLOYER_PRIVATE_KEY');
+        address deployerAddr = vm.addr(privateKey);
+
+        Prices prices = Prices(0xc01750713d34505171c3d66046D3b04C6fFb9cEC);
+
+        vm.startBroadcast(privateKey);
+
+        // Add deployer as valid price authority
+        prices.addValidSigner(deployerAddr);
+
+        // create a new feed to push prices to
+        uint256 feedId = prices.addFeed('HACKER FEED');
+
+        // set up encoding to push a price of 1.337 ether to the HACKER FEED
+        PriceDataWithoutSignature memory pdws = PriceDataWithoutSignature({
+            feedId: feedId,
+            nonce: 2,
+            timestamp: uint96(block.timestamp - 1 minutes),
+            price: 1.337 ether,
+            extraData: ''
+        });
+        bytes32 priceMessage = prices.getPriceMessage(
+            pdws.feedId, pdws.nonce, pdws.timestamp, pdws.price, pdws.extraData
+        );
+        (uint8 v, bytes32 r, bytes32 s) =
+            vm.sign(privateKey, ECDSA.toEthSignedMessageHash(priceMessage));
+        bytes memory signature = abi.encodePacked(r, s, v);
+        PriceData memory pd = PriceData({
+            signature: signature,
+            feedId: pdws.feedId,
+            nonce: pdws.nonce,
+            timestamp: pdws.timestamp,
+            price: pdws.price,
+            extraData: pdws.extraData
+        });
+        PriceData[] memory pds = new PriceData[](1);
+        pds[0] = pd;
+
+        uint256 totalFee = prices.totalFee();
+        // send the actual TX that pushes the price, paying 1 eth as fee
+        uint256 price = prices.getPrice{value: totalFee}(pds, '');
+
+        console.log('Oracle price %s', price);
+        vm.stopBroadcast();
+    }
+}
