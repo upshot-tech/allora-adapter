50e9742e1069a87d2fe964394384a147bb5d5392
br4e <milesminton@gmail.com>
2023-12-14 10:29:07 -0800

    update address

--- script/checkEthPriceFeed.ts
+++ script/checkEthPriceFeed.ts
@@ -5,7 +5,7 @@ import { UpshotAdapter } from '../types/UpshotAdapter'
 import { ethers } from 'ethers';
 import * as dotenv from 'dotenv';
 
-const UPSHOT_ADAPTER_ADDRESS = '0xdD3C703221c7F00Fe0E2d8cdb5403ca7760CDd4c'
+const UPSHOT_ADAPTER_ADDRESS = '0x4Bb814869573de58F3789FA1F1ed60A0Ad3c1A2e'
 const ETH_PRICE_FEED_TOPIC = 1
 const DUMMY_PRIVATE_KEY = '0x0123456789012345678901234567890123456789012345678901234567890123'
 
